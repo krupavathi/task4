ec4045f6ae8d7eebaa7014a6016c0bc1ee11a4a3
krupavathi <kputtaswamyostb2@gmail.com>
2019-11-25 10:46:03 +0530

    task3 copy

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Users from './Components/Users/Users';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    
+     <Users/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/State/State.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class State extends Component {
+// // state = {
+// // Users: [
+// // 10,
+// // }
+// // makeMeYounger = () => {
+// // const newState = this.state.Users.map((User) => {
+// // const tempUser = User;
+// // tempUser.age += 10;
+// // return tempUser;
+// // })
+// // console.log(newState);
+// // this.setState({ newState });
+// // }
+// // render() {
+// // return (
+// // <div>
+// // <button onClick={this.makeMeYounger}>make us 10 years younger</button>
+// // <br />
+// // <h1>{this.state.title}</h1>
+// // {
+// // this.state.Users.map((User) => (
+// // <div>User Age={User.age} name={User.name}</div>
+// // ))
+// // }
+// </div>
+// )
+// }
+// }
+
+
+export default State;
